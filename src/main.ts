9333eb8840ecb7a2d811494974becdd49109950a
Cristián Romo <cristian.g.romo@gmail.com>
2023-08-08 18:47:17 -0500

    Expand on GetOwnedGames request

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
-
+import { z } from "zod";
 
 async function parseConfig() {
   // Parse CLI/config/environment using yargs
@@ -11,7 +11,7 @@ async function parseConfig() {
   // - Template location?
   // - Output location?
 
-  const config = await (yargs(hideBin(process.argv))
+  return await (yargs(hideBin(process.argv))
     .option("steam-api-key", {
       alias: "k",
       demandOption: true,
@@ -27,20 +27,73 @@ async function parseConfig() {
     .env("TURBINE")
     .config()
     .argv);
-  console.log(config);
-  return config;
 }
 
-async function getOwnedGames(requestArguments: { steamApiKey: string; steamId: string; }) {
-  console.log("getOwnedGames", requestArguments);
+const baseOwnedGameSchema = z.object({
+  appid: z.number().int(),
+  playtime_forever: z.number().int(),
+  playtime_windows_forever: z.number().int(),
+  playtime_mac_forever: z.number().int(),
+  playtime_linux_forever: z.number().int(),
+  rtime_last_played: z.number().int(),
+  playtime_disconnected: z.number().int(),
+});
+const ownedGameWithAppInfoSchema = baseOwnedGameSchema.extend({
+  name: z.string(),
+  img_icon_url: z.string(),
+  // It seems that this may not be included with all games?
+  // content_descriptorids: z.number().int().array(),
+});
+const getBaseOwnedGamesResponseSchema = z.object({
+  response: z.object({
+    game_count: z.number().int(),
+    games: baseOwnedGameSchema.array(),
+  })
+});
+type GetBaseOwnedGamesResponse = z.infer<typeof getBaseOwnedGamesResponseSchema>;
+const getOwnedGamesWithAppInfoResponseSchema = z.object({
+  response: z.object({
+    game_count: z.number().int(),
+    games: ownedGameWithAppInfoSchema.array(),
+  })
+});
+type GetOwnedGamesWithAppInfoResponse = z.infer<typeof getOwnedGamesWithAppInfoResponseSchema>;
+
+type GetOwnedGamesRequest = {
+  steamApiKey: string;
+  steamId: string;
+  includeAppInfo?: boolean;
+  includePlayedFreeGames?: boolean;
+}
+
+async function getOwnedGames(requestArguments: Omit<GetOwnedGamesRequest, "includeAppInfo"> & { includeAppInfo?: false }): Promise<GetBaseOwnedGamesResponse>
+async function getOwnedGames(requestArguments: Omit<GetOwnedGamesRequest, "includeAppInfo"> & { includeAppInfo: true }): Promise<GetOwnedGamesWithAppInfoResponse>
+async function getOwnedGames(requestArguments: GetOwnedGamesRequest): Promise<GetBaseOwnedGamesResponse | GetOwnedGamesWithAppInfoResponse>
+{
   const response = await axios.get(
-    `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${requestArguments.steamApiKey}&steamid=${requestArguments.steamId}&format=json`
+    "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/",
+    {
+      params: {
+        key: requestArguments.steamApiKey,
+        steamid: requestArguments.steamId,
+        include_appinfo: requestArguments.includeAppInfo,
+        include_played_free_games: requestArguments.includePlayedFreeGames,
+        format: "json",
+      }
+    }
   );
-  console.log("response from steam", response);
+  return requestArguments.includeAppInfo ?
+    getOwnedGamesWithAppInfoResponseSchema.parse(response.data) :
+    getBaseOwnedGamesResponseSchema.parse(response.data);
 }
 
 async function main() {
-  await getOwnedGames(await parseConfig());
+  const games = await getOwnedGames({
+    ...await parseConfig(),
+    includeAppInfo: true,
+    includePlayedFreeGames: true,
+  });
+  console.log(games.response.games[0]);
 }
 
 main();
